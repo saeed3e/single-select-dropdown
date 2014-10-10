5fb928587b6717e649432d38ca24d4ff2bae3c71
Mohd Saeed <saeed3e@gmail.com>
2014-10-10 23:47:54 +0530

    update

--- j/singleSelect_DD.js
+++ j/singleSelect_DD.js
@@ -1,29 +1,6 @@
 /*jslint eqeq:true*/
 /*******Start of singleDD (version : v.2.1.0)*/
 
-var ieObj = {
-	scrollHandler : function(container, scrollContainer, firstElement, curActiveTouple){
-		if(curActiveTouple.length){
-			var scrollTopPos,
-				scrollContr 	=	container;
-			var maxHeight 		=	scrollContr.outerHeight();
-			var visible_top 	=	scrollContr.scrollTop();
-			var visible_bottom 	=	maxHeight + visible_top;
-			var high_top 		=	(curActiveTouple.position().top - scrollContainer.position().top) + firstElement.scrollTop();
-			var high_bottom 	=	high_top + curActiveTouple.outerHeight();
-
-			if (high_bottom >= visible_bottom){
-				scrollTopPos = (high_bottom - maxHeight) > 0 ? high_bottom - maxHeight : 0;
-				scrollContr.scrollTop(scrollTopPos);
-			} else if (high_top < visible_top){
-				scrollTopPos = high_top;
-				scrollContr.scrollTop(scrollTopPos);
-			}
-			return scrollTopPos;	
-		}		
-	}
-};
-
 (function ($) {
 	var curOpen;
 	$.fn.singleDD = function(opt){
@@ -55,6 +32,11 @@ var ieObj = {
 				inpWrap.append(_t.hidElm);					
 				elm.append(_t.dropCont);
 
+
+				_t.replaceData = function(data){
+					_t.dropCont.find('ul').html(_t.appendData(data));
+				}
+
 				elm.on('mouseenter',function(){
 					_t.currentActive = true;
 				}).on('mouseleave',function(){
@@ -67,22 +49,23 @@ var ieObj = {
 						inpWrap[0].focus();
 						_t.dropCont.css({'display':'block'});
 						$(this).parents('.singleDD').addClass('zIndexIE7');
-						opts.onOpen?opts.onOpen():'';
+						opts.onOpen?onOpen():'';
 					}
 				}).on('keydown',function(e){
 					var kCd = _t.keyCode(e),node;
 					var firstChild = _t.dropCont.find(':first-child');
 					var ulCont = _t.dropCont.find('ul');
+					var ulCont_hghtCont = ulCont.parents('.sDrop');
 					var ulCont_parent = ulCont.parent();
 
 					if(kCd==39 || kCd==40){
 						node = _t.nextSelection.call(_t,_t.currActiveItem);
 						_t.setValue.call(_t,node);
-						ieObj.scrollHandler(ulCont_parent,ulCont,firstChild,node);
+						ieObj.scrollHandler(ulCont_parent,ulCont_hghtCont,ulCont,firstChild,node);
 					}else if(kCd==37 || kCd==38){
 						node = _t.prevSelection.call(_t,_t.currActiveItem);
 						_t.setValue.call(_t,node);
-						ieObj.scrollHandler(ulCont_parent,ulCont,firstChild,node);
+						ieObj.scrollHandler(ulCont_parent,ulCont_hghtCont,ulCont,firstChild,node);
 					}else if(kCd==9 || kCd==13 || kCd == 27){
 						_t.currentActive =false;
 						_t.onblur(e,$(this));
@@ -101,13 +84,16 @@ var ieObj = {
 				_t.dropCont.on('click','li',function(){ //Bind click event on each suggestion/options
 					var val = $(this).text();
 					var id = _t.remDelimiter($(this).attr('id'));
-					if(id){
-						_t.inpTextElm.val(val);
-						_t.hidElm.attr({'value':id});
-					}else{
+
+					if($(this).index()==0){
 						_t.inpTextElm.val('');
-						_t.hidElm.attr({'value':''});
+						_t.hidElm.attr({'value':''});	
+					}					
+					else{
+						_t.inpTextElm.val(val);
+						_t.hidElm.attr({'value':id});	
 					}
+					
 					_t.dropCont.css({'display':'none'});
 					inpWrap[0].focus();
 					if(opts.callBack)opts.callBack(id);
@@ -116,7 +102,7 @@ var ieObj = {
 					$(this).addClass('sAct');
 				}).on('mouseout','li',function(){
 					$(this).removeClass('sAct');
-				}).html(_t.appendData.call(_t)); // fill data in dropdown;
+				}).html(_t.appendData(opts.data)); // fill data in dropdown;
 
 
 				_t.prefillData(opts,id);	// set prefill value
@@ -143,7 +129,7 @@ var ieObj = {
 						_t.dropCont.css({'display':'none'});	
 						_t.enableScroll(e);
 						node.parents('.singleDD').removeClass('zIndexIE7');
-						opts.onClose?opts.onClose():'';
+						opts.onClose?onClose():'';
 					}
 				},
 				setValue: function(node){
@@ -154,10 +140,10 @@ var ieObj = {
 						prototype_Objects.setVal_inHiddenField.call(this,'','');
 					}
 				},
-				appendData : function(){
+				appendData : function(data){
 					var li='';
-					for(var x in opts.data){
-						li += '<li id="'+x+'">'+opts.data[x]+'</li>';
+					for(var x in data){
+						li += '<li id="'+x+'">'+data[x]+'</li>';
 					}
 					return '<ul>'+li+'</ul>';
 				},
@@ -181,6 +167,7 @@ var ieObj = {
 				enableScroll : function(e){
 					$(window).off('keydown', prototype_Objects.disb_Scroll_handler);	// to enable window scroll
 				},
+				
 
 				nextSelection : function(elm){
 					var nextNode = elm.next();
